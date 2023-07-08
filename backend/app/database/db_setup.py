827d8f1aa88823d9b16beaf6789d670b2129c863
ibrahzizo360 <92813019+ibrahzizo360@users.noreply.github.com>
2023-07-08 07:38:36 +0000

    commit

--- backend/app/database/db_setup.py
+++ backend/app/database/db_setup.py
@@ -1,18 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,scoped_session
-from sqlalchemy.orm import Session
 from app.config import settings
 from sqlmodel import SQLModel
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 engine = create_engine(settings.DATABASE_URL, echo=True)
-db = scoped_session(sessionmaker(bind=engine))
-metadata = db.metadata
+session_factory   = sessionmaker(autocommit = False, autoflush = False, bind=engine)
+db = scoped_session(session_factory)
 
 
 def create_database():
-    return SQLModel.metadata.create_all(bind=engine)
+    return SQLModel.metadata.create_all(bind = engine)
 
 
 
